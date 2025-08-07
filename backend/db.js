1a7fd330c9f206d6e13f93565397ede1275552df
suraj-bv <surajbv5566@gmail.com>
2025-08-07 22:31:20 +0530

    added .env for db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mysql",  // change to your MySQL root password
-  database: "task_manager"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect(err => {
   if (err) throw err;
